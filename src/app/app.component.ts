e294a30274a61badef01cd0b745b8b65f4f7579f
nigel2000 <d.jones560@ntlworld.com>
2023-07-26 19:04:12 +0100

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Cart } from './pages/models/cart.model';
+import { CartService } from './services/cart.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent implements OnInit {
+  cart: Cart = { items: [] };
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit() {
+    this.cartService.cart.subscribe((_cart) => {
+      this.cart = _cart;
+    });
+  }
+}
